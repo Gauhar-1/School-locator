1d39412cc2329e063b3d80b448305675ccd33937
Gauhar-1 <mustakkhan1210@gmail.com>
2025-05-25 19:29:03 +0530

    env changed 2

--- utils/distance.js
+++ utils/distance.js
@@ -1,7 +1,7 @@
 export  const calculateDistance = (lat1, lon1, lat2, lon2) => {
   const toRad = degree => degree * (Math.PI / 180);
   const R = 6371; 
-  const dLat = toRad(lat2 - lat1);
+  const dLat = toRad(lat2 - lat1)
   const dLon = toRad(lon2 - lon1);
 
   const a =
